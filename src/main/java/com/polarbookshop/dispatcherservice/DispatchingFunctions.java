87d573f3fa78745f420c63eae4d23f2461e65ad0
stk346 <stk346@gmail.com>
2024-09-11 07:13:58 +0900

    feat: ch10.3.1 스프링 클라우드 함수의 함수적 패러다임 사용

---
+++ src/main/java/com/polarbookshop/dispatcherservice/DispatchingFunctions.java
@@ -0,0 +1,33 @@
+package com.polarbookshop.dispatcherservice;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Flux;
+
+import java.util.function.Function;
+
+@Configuration
+public class DispatchingFunctions {
+    private static final Logger log = LoggerFactory.getLogger(DispatchingFunctions.class);
+
+    // 주문을 포장하는 비즈니스 로직을 구현하는 함수
+    @Bean // 빈으로 정의된 함수는 스프링 클라우드 함수가 찾아서 관리할 수 있다. 빈으로 등록된 함수는 스프링 함수 프래임워크에 의해 추가 기능을 갖는다.
+    public Function<OrderAcceptedMessage, Long> pack() {
+        return orderAcceptedMessage -> {
+            log.info("The order with id {} is packed.",
+                    orderAcceptedMessage.orderId());
+            return orderAcceptedMessage.orderId();
+        };
+    }
+
+    // 주문의 아이디(Long 타입)을 입력 받고 OrderDispatchedMessage를 반환한다.
+    @Bean
+    public Function<Flux<Long>, Flux<OrderDispatcedMessage>> label() {
+        return orderFlux -> orderFlux.map(orderId -> {
+            log.info("The order with id {} is labeled.", orderId);
+            return new OrderDispatcedMessage(orderId);
+        });
+    }
+}
